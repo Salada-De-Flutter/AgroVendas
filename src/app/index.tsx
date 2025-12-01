08386b745990d35fd20100422ae4fc11f45f5b5b
Salada-De-Flutter <fluttered002@gmail.com>
2025-12-01 12:12:16 -0300

    Primeiro Commit

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function IndexScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Página Inicial</Text>
+    </View>
+  );
+}
